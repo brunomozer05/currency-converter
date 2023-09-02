6fcfc66adfc9a60932c1ed43ba469cc2d045ecd2
Bruno Merico Mozer <brunomozer05@gmail.com>
2023-09-02 02:06:18 -0300

    working select options

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,20 @@
 const fetchcoins = async () => {
     const url = 'https://economia.awesomeapi.com.br/last/'
-    const coins = 'USD-BRL'
 
-    const currency = await fetch(url + coins)
+    var select = document.getElementById("option1"); 
+    var selectedOption1 = select.options[select.selectedIndex]; 
+    console.log(selectedOption1.value);
+
+    var select = document.getElementById("option2"); 
+    var selectedOption2 = select.options[select.selectedIndex];
+    console.log(selectedOption2.value);
+    
+    const coins1 = (selectedOption1.value)
+    const coins2 = (selectedOption2.value)
+
+    const currency = await fetch(url + coins1 + "-" + coins2)
         .then(currency => currency.json())
-        .then(coins => coins.USDBRL.bid)
+        .then (coins => coins[coins1 + coins2].bid)
 
     const inputValue = document.getElementById("value").value
 
