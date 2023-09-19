f4b133b0d506577f88c87aa3ef6fe59305c50598
Bruno Merico Mozer <brunomozer05@gmail.com>
2023-09-19 12:03:39 -0300

    CSS and JS Formatted

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const fetchcoins = async () => {
-    const url = 'https://economia.awesomeapi.com.br/last/'
+  const url = "https://economia.awesomeapi.com.br/last/";
 
-    var select = document.getElementById("option1"); 
-    var selectedOption1 = select.options[select.selectedIndex]; 
-    console.log(selectedOption1.value);
+  var select = document.getElementById("option1");
+  var selectedOption1 = select.options[select.selectedIndex];
+  console.log(selectedOption1.value);
 
-    var select = document.getElementById("option2"); 
-    var selectedOption2 = select.options[select.selectedIndex];
-    console.log(selectedOption2.value);
-    
-    const coins1 = (selectedOption1.value)
-    const coins2 = (selectedOption2.value)
-    
-    let currency = 1
+  var select = document.getElementById("option2");
+  var selectedOption2 = select.options[select.selectedIndex];
+  console.log(selectedOption2.value);
 
-    if (coins1 !== coins2) {    
-        currency = await fetch(url + coins1 + "-" + coins2)
-        .then(currency => currency.json())
-        .then (coins => coins[coins1 + coins2].bid)
-    }
-        const inputValue = document.getElementById("value").value
-        
-        console.log(currency)
-        
-        const result = document.getElementById("result")
-        result.innerHTML = (currency * inputValue).toLocaleString('pt-BR', {style: 'currency', currency: coins2});
-    
-}
+  const coins1 = selectedOption1.value;
+  const coins2 = selectedOption2.value;
+
+  let currency = 1;
+
+  if (coins1 !== coins2) {
+    currency = await fetch(url + coins1 + "-" + coins2)
+      .then((currency) => currency.json())
+      .then((coins) => coins[coins1 + coins2].bid);
+  }
+  const inputValue = document.getElementById("value").value;
+
+  console.log(currency);
+
+  const result = document.getElementById("result");
+  result.innerHTML = (currency * inputValue).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: coins2,
+  });
+};
